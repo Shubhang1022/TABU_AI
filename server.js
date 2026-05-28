80fca0fe2c29eda90753f1c851754b6046b9bf4c
shubhang1022 <mishrashanu233@gmail.com>
2026-05-28 14:35:22 +0530

    fix: use native fetch, remove node-fetch, pin uuid@9 for Node 24 compat

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const fetch = require('node-fetch');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
